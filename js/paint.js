da0c2cda6d6920b5693e0994b8b9306a4f4ec0f4
Aaron Gilman <gilmanaa2@gmail.com>
2019-05-30 16:49:39 +0300

    update for mvp

--- js/paint.js
+++ js/paint.js
@@ -4,11 +4,12 @@ var greenColor = document.querySelector('.green');
 var blueColor = document.querySelector('.blue');
 var yellowColor = document.querySelector('.yellow');
 var purpleColor = document.querySelector('.purple');
-var pinkColor = document.querySelector('.pink');
+var blackColor = document.querySelector('.black');
 var eraserFeature = document.querySelector('.eraser');
 var clearFeature = document.querySelector('.clear');
 var paintId = 0;
 var clickCheck;
+var paintColor = 'black';
 
 function painting(e) {
     if (clickCheck === 1) {
@@ -16,14 +17,14 @@ function painting(e) {
         var posX = e.clientX;
         var posY = e.clientY;
         var paint = document.createElement('div')
-        paint.className = "painting";
+        paint.className = `painting ${paintColor}`;
         paint.id = paintId;
         paint.style = `left: ${posX}px; top: ${posY}px`;
         canvas.appendChild(paint);
     }
 }
 function clickDown() {
-        canvas.addEventListener('mousemove',painting); 
+    canvas.addEventListener('mousemove',painting); 
 }
 canvas.addEventListener('mousedown',function() {
     clickCheck = 1;
@@ -32,3 +33,36 @@ canvas.addEventListener('mousedown',function() {
 canvas.addEventListener('mouseup',function() {
     clickCheck = 0;
 })
+
+// function changeColor() {
+//     var colors = document.getElementsByClassName('color');
+//     for (var i = 0; i < colors.length; i++) {
+//         console.log(colors.className);
+//     }
+//    paintColor = 
+// }
+// changeColor();
+
+function changeRed() {paintColor = 'red';}
+function changeGreen() {paintColor = 'green';}
+function changeBlue() {paintColor = 'blue';}
+function changeYellow() {paintColor = 'yellow';}
+function changePurple() {paintColor = 'purple';}
+function changeBlack() {paintColor = 'black';}
+function eraser() {paintColor = 'eraser'}
+function clearScreen() {
+    for (var i = 1; i <= paintId; i++) {
+        var remove = document.getElementById(`${i}`);
+        canvas.removeChild(remove);
+    }
+    paintId = 0;
+}
+
+redColor.addEventListener('click',changeRed);
+greenColor.addEventListener('click',changeGreen);
+blueColor.addEventListener('click',changeBlue);
+yellowColor.addEventListener('click',changeYellow);
+purpleColor.addEventListener('click',changePurple);
+blackColor.addEventListener('click',changeBlack);
+eraserFeature.addEventListener('click',eraser);
+clearFeature.addEventListener('click',clearScreen);
